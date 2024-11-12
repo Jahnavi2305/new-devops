c43e0c26eebf1763733d39c9f8a80f1ea889c430
jahnavi <awsdevopsjanu555@gmail.com>
2024-11-12 23:32:57 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -5,7 +5,7 @@ echo "please enter name"
 read -s USERNAME #takes input from termainal
 
 echo " the name is  $USERNAME"
-
+echo "please enter password"
 read -s PASSWORD
 
 echo " the password is : $PASSWORD"
