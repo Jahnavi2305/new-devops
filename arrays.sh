992127384aa00076153fdab64a1f12154c38c0e3
jahnavi <awsdevopsjanu555@gmail.com>
2024-11-12 23:38:45 +0530

    arrays

---
+++ arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("kIWI" "ORANGE" "APPLE")
+
+echo " first fruit is ${FRUITS[0]}"
+echo " second fruit is ${FRUITS[1]}"
+echo "third fruit is ${FRUIT[2]}"
+echo " first fruit is ${FRUITS[@]}"
+
